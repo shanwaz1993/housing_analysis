a3184e9db2537a3d8061e5cf15a904b90bc5e860
shanwaz1993 <shanwaz1993@gmail.com>
2019-05-12 00:24:49 +0530

    Basic operation on CSV file

--- read_csv.py
+++ read_csv.py
@@ -3,13 +3,48 @@
 import pandas as pd
 
 data=pd.read_csv(r'/home/shan/Downloads/Melbourne_housing_FULL.csv',index_col=0)
-#print(data.describe())
+
+#take a look into data
+print(data.describe())
 #print(data.info)
-
-#print("Sample data")
 #print(data.head())
+#print(data.columns)
+#print(data.index)
+#print(data.dtypes)
 
+#-----------categorization of data 
 #print(data.isnull().any())
-print(data.dtypes)
-df=data.select_dtypes(include='object')
-print(df.columns)
+#df=data.select_dtypes(include='object')
+#print(df.head())
+#print(df.isnull().any())
+
+#----------Measure of tandency 
+#print(data['Regionname'].mode())
+#print(df['CouncilArea'].mode())
+#print(data['Price'].mean())
+#print(data['Price'].median())
+
+
+#--------measure of spreads
+print(data['Price'].std())
+print(data['Price'].var())
+print(data['Price'].mad())
+
+
+#print(data.mode())
+#print(data[data.Rooms==1].loc[:,'Rooms':'Price'].count())
+#print(data.loc[data.Rooms==1].Price)
+#print(data[data.Rooms==2].loc[:,'Rooms':'Price'].count())
+#print(data[data.Rooms==3].loc[:,'Rooms':'Price'].count())
+
+#filter
+#print(data[(data.Rooms==1) & (data.Price>2000000)].loc[:,'Rooms':'Price'])
+#print(data[(data.Rooms==1) & (data.Price>2000000)].loc[:,'Rooms':'Price'].sort_values(['Price'],ascending=[0]))
+#print(data[(data.Rooms==1) & (data.Price>2000000)].loc[:,'Rooms':'Price'].sort_values(['Rooms','Type','Price'],ascending=[0,1,1]))
+
+#print(data[(data.Rooms==1) | (data.Rooms>2) & (data.Type=='u')].loc[:,'Rooms':'Price'])
+
+#print(data[(data.Rooms!=3)&(data.Price>2000000)&(data.Type!='h')].loc[:,'Rooms':'Price'])
+
+
+
